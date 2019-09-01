da316749c334cb3615bd1dfeab759df35f6f0239
Liviu Cira <Liviu.Cira@catalysts.cc>
2019-09-01 12:18:08 +0300

    Added Authority entity

---
+++ src/main/java/warehouse/entity/Authority.java
@@ -0,0 +1,24 @@
+package warehouse.entity;
+
+import warehouse.role.Role;
+
+import javax.persistence.*;
+
+@Entity
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public Authority() {
+    }
+
+    public Authority(Role name) {
+        this.role = name;
+    }
+    // getters and setters
+}
