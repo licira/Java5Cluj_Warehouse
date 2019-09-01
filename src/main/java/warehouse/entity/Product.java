7229c3c2448731e78b46645e70a86753b65f41e5
Adi <ady_de_bocy@yahoo.com>
2019-09-01 11:43:00 +0300

    Add setter for ID

--- src/main/java/warehouse/entity/Product.java
+++ src/main/java/warehouse/entity/Product.java
@@ -21,6 +21,10 @@ public class Product {
         this.description = description;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public long getId() {
         return id;
     }
